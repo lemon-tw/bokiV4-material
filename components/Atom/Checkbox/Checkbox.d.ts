660d7ac0b5546d8df53a399b842ea44d8c508c2b
Lemon-Danny <Lemon-Danny@users.noreply.github.com>
2025-06-11 09:28:28 +0000

    deploy: 5cf19459661c357aa7734e554e4adb484cfacbb9

--- components/Atom/Checkbox/Checkbox.d.ts
+++
@@ -1,9 +0,0 @@
-import { default as MuiCheckbox } from '@mui/material/Checkbox';
-import { Theme } from '@mui/material/styles';
-import { palette as defaultPalette } from '../../../themes/defaultPalette';
-export declare const getcbStyles: (theme: Theme, color?: keyof typeof defaultPalette) => {
-    bgColor: any;
-    fontColor: any;
-    overlay: any;
-};
-export declare const Checkbox: ({ ...props }: typeof MuiCheckbox) => import("react/jsx-runtime").JSX.Element;
--- stories/checkbox/Checkbox.stories.d.ts
+++
@@ -1,6 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react';
-import { Checkbox } from '../../components/Atom/Checkbox/Checkbox';
-declare const meta: Meta<typeof Checkbox>;
-export default meta;
-type Story = StoryObj<typeof Checkbox>;
-export declare const Primary: Story;
