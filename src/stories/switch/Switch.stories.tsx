52e47f4e52754253a5dd97b57e6ef10638e8770a
Danny6401 <167506458+Danny6401@users.noreply.github.com>
2025-06-11 17:27:04 +0800

    complete Switch

---
+++ src/stories/switch/Switch.stories.tsx
@@ -0,0 +1,50 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+
+import { Switch } from "../../components/Atom/Switch/Switch";
+import Done from "@mui/icons-material/Done";
+import Close from "@mui/icons-material/Close";
+
+const meta: Meta<typeof Switch> = {
+  title: "components/Switch",
+  component: Switch,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    selectedIcon: {
+      control: false,
+    },
+    unselectedIcon: {
+      control: false,
+    },
+    size: {
+      options: ["small", "medium", "large"],
+      control: { type: "radio" },
+    },
+  },
+  args: {
+    onClick: fn(),
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Switch>;
+
+export const NormalSwitch: Story = {
+  args: {
+    disabled: false,
+    size: "medium",
+  },
+};
+
+export const IconSwitch: Story = {
+  args: {
+    disabled: false,
+    selectedIcon: <Done />,
+    unselectedIcon: <Close />,
+    size: "small",
+  },
+};
