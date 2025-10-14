e2ffb3cb00b80c3fcaec2ae184a5d7d2a512ad15
Danny6401 <167506458+Danny6401@users.noreply.github.com>
2025-10-14 15:13:50 +0800

    Updates

---
+++ assets/CodeInput.stories-BHmsQda3.js
@@ -0,0 +1,38 @@
+import{j as e}from"./jsx-runtime-BO8uF4Og.js";import{r}from"./index-D4H_InIO.js";import"./DefaultPropsProvider-DN8Xv8n1.js";import{C as m,B as E,a as N,T as a}from"./Typography-Dy6dv8OK.js";import"./index-BkuToD7M.js";import"./index-vYCkCKEW.js";import"./tiny-invariant-CopsF_GD.js";const A={title:"components/Atoms/CodeInput/CodeInput",component:m,parameters:{layout:"centered",controls:{exclude:["promoDetail","setCode","setRoomCode"]}},tags:["autodocs"],argTypes:{roomCode:{control:{type:"array"}},showRate:{control:{type:"boolean"}},showVoucher:{control:{type:"boolean"}}},args:{}},$=[{memberOnly:!0,code:"DOUBLE11",title:"雙11限時優惠｜現折 NT$500",discountAmount:"TWD500",validPeriod:"2025/11/10 - 2025/11/30",stayPeriod:"2025/11/10 - 2026/11/10",minSpend:"TWD 5,000",description:"2025年11月1日至11日限時開跑，雙11專屬優惠現折 NT$500！超值折扣只有這 11 天，錯過就要再等一年，立即搶訂！"},{memberOnly:!1,code:"DOUBLE12",title:"雙11限時優惠｜現折 NT$500雙11限時優惠｜現折 NT$500雙11限時優惠｜現折 NT$500雙11限時優惠｜現折 NT$500雙11限時優惠｜現折 NT$500",discountAmount:"TWD500",validPeriod:"2025/11/10 - 2025/11/30",stayPeriod:"2025/11/10 - 2026/11/10",minSpend:"TWD 5,000",description:"2025年11月1日至11日限時開跑，雙11專屬優惠現折 NT$500！超值折扣只有這 11 天，錯過就要再等一年，立即搶訂！"},{memberOnly:!0,code:"DOUBLE13",title:"雙11限時優惠｜現折 NT$500",discountAmount:"TWD500",validPeriod:"2025/11/10 - 2025/11/30",stayPeriod:"2025/11/10 - 2026/11/10",minSpend:"TWD 5,000",description:"2025年11月1日至11日限時開跑，雙11專屬優惠現折 NT$500！超值折扣只有這 11 天，錯過就要再等一年，立即搶訂！"},{memberOnly:!0,code:"DOUBLE13",title:"雙11限時優惠｜現折 NT$500",discountAmount:"TWD500",validPeriod:"2025/11/10 - 2025/11/30",stayPeriod:"2025/11/10 - 2026/11/10",minSpend:"TWD 5,000",description:"2025年11月1日至11日限時開跑，雙11專屬優惠現折 NT$500！超值折扣只有這 11 天，錯過就要再等一年，立即搶訂！"},{memberOnly:!0,code:"DOUBLE11",title:"雙11限時優惠｜現折 NT$500",discountAmount:"TWD500",validPeriod:"2025/11/10 - 2025/11/30",stayPeriod:"2025/11/10 - 2026/11/10",minSpend:"TWD 5,000",description:"2025年11月1日至11日限時開跑，雙11專屬優惠現折 NT$500！超值折扣只有這 11 天，錯過就要再等一年，立即搶訂！"},{memberOnly:!1,code:"DOUBLE12",title:"雙11限時優惠｜現折 NT$500",discountAmount:"TWD500",validPeriod:"2025/11/10 - 2025/11/30",stayPeriod:"2025/11/10 - 2026/11/10",minSpend:"TWD 5,000",description:"2025年11月1日至11日限時開跑，雙11專屬優惠現折 NT$500！超值折扣只有這 11 天，錯過就要再等一年，立即搶訂！"},{memberOnly:!0,code:"DOUBLE13",title:"雙11限時優惠｜現折 NT$500",discountAmount:"TWD500",validPeriod:"2025/11/10 - 2025/11/30",stayPeriod:"2025/11/10 - 2026/11/10",minSpend:"TWD 5,000",description:"2025年11月1日至11日限時開跑，雙11專屬優惠現折 NT$500！超值折扣只有這 11 天，錯過就要再等一年，立即搶訂！"},{memberOnly:!0,code:"DOUBLE11",title:"雙11限時優惠｜現折 NT$500",discountAmount:"TWD500",validPeriod:"2025/11/10 - 2025/11/30",stayPeriod:"2025/11/10 - 2026/11/10",minSpend:"TWD 5,000",description:"2025年11月1日至11日限時開跑，雙11專屬優惠現折 NT$500！超值折扣只有這 11 天，錯過就要再等一年，立即搶訂！"},{memberOnly:!1,code:"DOUBLE12",title:"雙11限時優惠｜現折 NT$500",discountAmount:"TWD500",validPeriod:"2025/11/10 - 2025/11/30",stayPeriod:"2025/11/10 - 2026/11/10",minSpend:"TWD 5,000",description:"2025年11月1日至11日限時開跑，雙11專屬優惠現折 NT$500！超值折扣只有這 11 天，錯過就要再等一年，立即搶訂！"},{memberOnly:!0,code:"DOUBLE13",title:"雙11限時優惠｜現折 NT$500",discountAmount:"TWD500",validPeriod:"2025/11/10 - 2025/11/30",stayPeriod:"2025/11/10 - 2026/11/10",minSpend:"TWD 5,000",description:"2025年11月1日至11日限時開跑，雙11專屬優惠現折 NT$500！超值折扣只有這 11 天，錯過就要再等一年，立即搶訂！"}],t={render:u=>{const[s,d]=r.useState(null),[i,T]=r.useState(["STD","DELUXE"]),[y,D]=r.useState([""]),h=o=>{d(o.currentTarget)},C=()=>{d(null)},x=!!s;return e.jsxs(e.Fragment,{children:[e.jsx(E,{onClick:h,children:"Open popover"}),e.jsx(m,{open:x,anchorEl:s,roomCode:i,setRoomCode:T,onClose:C,setCode:D,...u}),e.jsxs(N,{sx:{position:"absolute",top:"50px",left:"150px",borderRadius:"8px",border:"1px solid #1994FC",p:2},children:[i.map((o,n)=>e.jsxs(a,{children:["Using room code ",n+1,": ",o]},n)),y.map((o,n)=>e.jsxs(a,{children:["Using codes ",n+1,": ",o]},n))]})]})},args:{promoDetail:$,showRate:!0,showVoucher:!0}};var l,c,p;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: args => {
+    const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+    const [roomCode, setRoomCode] = useState(["STD", "DELUXE"]);
+    const [code, setCode] = useState([""]);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+    const open = Boolean(anchorEl);
+    return <>\r
+        <Button onClick={handleClick}>Open popover</Button>\r
+        <CodeInput open={open} anchorEl={anchorEl} roomCode={roomCode} setRoomCode={setRoomCode} onClose={handleClose} setCode={setCode} {...args} />\r
+        <Box sx={{
+        position: "absolute",
+        top: "50px",
+        left: "150px",
+        borderRadius: "8px",
+        border: "1px solid #1994FC",
+        p: 2
+      }}>\r
+          {roomCode.map((item, index) => <Typography key={index}>\r
+              Using room code {index + 1}: {item}\r
+            </Typography>)}\r
+          {code.map((item, index) => <Typography key={index}>\r
+              Using codes {index + 1}: {item}\r
+            </Typography>)}\r
+        </Box>\r
+      </>;
+  },
+  args: {
+    promoDetail: promoDetail,
+    showRate: true,
+    showVoucher: true
+  }
+}`,...(p=(c=t.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};const U=["ShowAllProps"];export{t as ShowAllProps,U as __namedExportsOrder,A as default};
