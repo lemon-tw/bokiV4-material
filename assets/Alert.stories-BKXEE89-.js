dfbb68c69fd680e0ee51fe6231700cffc7e8ba72
Atsushi <newhandarky@gmail.com>
2026-02-03 13:57:21 +0800

    Updates

---
+++ assets/Alert.stories-BKXEE89-.js
@@ -0,0 +1,74 @@
+import{j as t}from"./jsx-runtime-BO8uF4Og.js";import{r as e,R as C}from"./index-D4H_InIO.js";import"./lemonTheme-B7Qis2N-.js";import"./Rest-Calpe_Rv.js";import{A as s}from"./Alert-C3r4vYPZ.js";import"./Avatar-pRnr6DU5.js";import{B as r}from"./ToggleButton-1S1uagp-.js";import"./DraggableChips-Gzah4u51.js";import{e as Z}from"./Modal-BPmM9JPO.js";import"./BottomModal-imHOmNSj.js";import"./Profile-CdAcNaFX.js";import"./Typography-BZQ6nlo4.js";import{p as y,I as j}from"./CheckCircle.es-BXMbO79g.js";import"./defaultPalette-DLKPgRCf.js";import"./dayjs.min-BJoxDa44.js";import"./zh-tw-C-UY_gDz.js";import"./Cart-z4MSENyy.js";import"./FormControlLabel-D0F9NOZF.js";import"./Plus.es-B6v6yg-F.js";import"./Moon.es-CJIcfKlP.js";import"./Users.es-DGjqvHPG.js";import"./WarningCircle.es-x_3DymOb.js";import"./index-Bm8UbI65.js";import"./useTheme-B9a50qyY.js";import"./useSlotProps-BPBm8KYw.js";import"./Link-CuGHZV3F.js";import"./index-BfevDTIX.js";import"./getThemeProps-MPmnPgUq.js";import"./CaretUp.es-B8wKLcLK.js";import"./MagnifyingGlassPlus.es-CjjA4OJ9.js";import"./Tabs-BBXbhsQj.js";import"./CrownSimple.es-N3wLHKBe.js";import"./Warning.es-BRl635yR.js";import"./Divider-DR1e4bhK.js";import"./Collapse-DpSy8Sr3.js";import"./Copy.es-Cj1k5QIq.js";import"./index-BkuToD7M.js";import"./index-vYCkCKEW.js";import"./tiny-invariant-CopsF_GD.js";const B=new Map([["bold",e.createElement(e.Fragment,null,e.createElement("path",{d:"M225.6,62.64l-88-48.17a19.91,19.91,0,0,0-19.2,0l-88,48.17A20,20,0,0,0,20,80.19v95.62a20,20,0,0,0,10.4,17.55l88,48.17a19.89,19.89,0,0,0,19.2,0l88-48.17A20,20,0,0,0,236,175.81V80.19A20,20,0,0,0,225.6,62.64ZM128,36.57,200,76,128,115.4,56,76ZM44,96.79l72,39.4v76.67L44,173.44Zm96,116.07V136.19l72-39.4v76.65Z"}))],["duotone",e.createElement(e.Fragment,null,e.createElement("path",{d:"M128,129.09V232a8,8,0,0,1-3.84-1l-88-48.16a8,8,0,0,1-4.16-7V80.2a8,8,0,0,1,.7-3.27Z",opacity:"0.2"}),e.createElement("path",{d:"M223.68,66.15,135.68,18h0a15.88,15.88,0,0,0-15.36,0l-88,48.17a16,16,0,0,0-8.32,14v95.64a16,16,0,0,0,8.32,14l88,48.17a15.88,15.88,0,0,0,15.36,0l88-48.17a16,16,0,0,0,8.32-14V80.18A16,16,0,0,0,223.68,66.15ZM128,32h0l80.34,44L128,120,47.66,76ZM40,90l80,43.78v85.79L40,175.82Zm96,129.57V133.82L216,90v85.78Z"}))],["fill",e.createElement(e.Fragment,null,e.createElement("path",{d:"M223.68,66.15,135.68,18a15.88,15.88,0,0,0-15.36,0l-88,48.17a16,16,0,0,0-8.32,14v95.64a16,16,0,0,0,8.32,14l88,48.17a15.88,15.88,0,0,0,15.36,0l88-48.17a16,16,0,0,0,8.32-14V80.18A16,16,0,0,0,223.68,66.15ZM128,120,47.65,76,128,32l80.35,44Zm8,99.64V133.83l80-43.78v85.76Z"}))],["light",e.createElement(e.Fragment,null,e.createElement("path",{d:"M222.72,67.9l-88-48.17a13.9,13.9,0,0,0-13.44,0l-88,48.18A14,14,0,0,0,26,80.18v95.64a14,14,0,0,0,7.28,12.27l88,48.18a13.92,13.92,0,0,0,13.44,0l88-48.18A14,14,0,0,0,230,175.82V80.18A14,14,0,0,0,222.72,67.9ZM127,30.25a2,2,0,0,1,1.92,0L212.51,76,128,122.24,43.49,76ZM39,177.57a2,2,0,0,1-1-1.75V86.66l84,46V223Zm177.92,0L134,223V132.64l84-46v89.16A2,2,0,0,1,217,177.57Z"}))],["regular",e.createElement(e.Fragment,null,e.createElement("path",{d:"M223.68,66.15,135.68,18h0a15.88,15.88,0,0,0-15.36,0l-88,48.17a16,16,0,0,0-8.32,14v95.64a16,16,0,0,0,8.32,14l88,48.17a15.88,15.88,0,0,0,15.36,0l88-48.17a16,16,0,0,0,8.32-14V80.18A16,16,0,0,0,223.68,66.15ZM128,32h0l80.34,44L128,120,47.66,76ZM40,90l80,43.78v85.79L40,175.82Zm96,129.57V133.82L216,90v85.78Z"}))],["thin",e.createElement(e.Fragment,null,e.createElement("path",{d:"M221.76,69.66l-88-48.18a12,12,0,0,0-11.52,0l-88,48.18A12,12,0,0,0,28,80.18v95.64a12,12,0,0,0,6.24,10.52l88,48.18a11.95,11.95,0,0,0,11.52,0l88-48.18A12,12,0,0,0,228,175.82V80.18A12,12,0,0,0,221.76,69.66ZM126.08,28.5a3.94,3.94,0,0,1,3.84,0L216.67,76,128,124.52,39.33,76Zm-88,150.83A4,4,0,0,1,36,175.82V83.29l88,48.16v94.91Zm179.84,0-85.92,47V131.45l88-48.16v92.53A4,4,0,0,1,217.92,179.32Z"}))]]),m=e.forwardRef((n,i)=>e.createElement(y,{ref:i,...n,weights:B}));m.displayName="CubeIcon";const me={title:"components/Modules/Alert/Alert",component:s,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{color:{table:{disable:!0}}},args:{}},o={render:n=>t.jsxs(s,{onClose:()=>{},...n,sx:{mb:2},children:[n.children,t.jsxs("div",{style:{display:"flex",justifyContent:"flex-end"},children:[t.jsx(r,{variant:"text",children:"Close"}),t.jsx(r,{variant:"text",children:"Retry"})]})]}),args:{severity:"success",alertTitle:"Alert title here",children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."}},l={render:n=>t.jsx(s,{icon:t.jsx(m,{}),onClose:()=>{},action:t.jsxs("div",{children:[t.jsx(r,{variant:"text",sx:{height:"35px"},children:"Close"}),t.jsx(r,{sx:{height:"35px"},children:"Retry"}),t.jsx(j,{color:"inherit",size:"small",children:t.jsx(Z,{fontSize:"inherit"})})]}),...n,children:n.children}),args:{severity:"info",children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit"}},a={render:n=>{const[i,c]=C.useState(!0);return t.jsxs("div",{children:[i&&t.jsx(s,{icon:t.jsx(m,{}),action:t.jsxs("div",{children:[t.jsx(r,{variant:"text",sx:{height:"35px"},onClick:()=>{c(!1)},children:"Close"}),t.jsx(j,{color:"inherit",size:"small",onClick:()=>{c(!1)},children:t.jsx(Z,{fontSize:"inherit"})})]}),...n,children:n.children}),!i&&t.jsx(r,{onClick:()=>{c(!0)},children:"re-open"})]})},args:{severity:"error",children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit"}};var p,d,u;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: args => {
+    return <Alert onClose={() => {}} {...args} sx={{
+      mb: 2
+    }}>
+        {args.children}
+        <div style={{
+        display: "flex",
+        justifyContent: "flex-end"
+      }}>
+          <Button variant="text">Close</Button>
+          <Button variant="text">Retry</Button>
+        </div>
+      </Alert>;
+  },
+  args: {
+    severity: "success",
+    alertTitle: "Alert title here",
+    children: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+  }
+}`,...(u=(d=o.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var h,x,v;l.parameters={...l.parameters,docs:{...(h=l.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: args => {
+    return <Alert icon={<CubeIcon />} onClose={() => {}} action={<div>
+            <Button variant="text" sx={{
+        height: "35px"
+      }}>
+              Close
+            </Button>
+            <Button sx={{
+        height: "35px"
+      }}>Retry</Button>
+            <IconButton color="inherit" size="small">
+              <XIcon fontSize="inherit" />
+            </IconButton>
+          </div>} {...args}>
+        {args.children}
+      </Alert>;
+  },
+  args: {
+    severity: "info",
+    children: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+  }
+}`,...(v=(x=l.parameters)==null?void 0:x.docs)==null?void 0:v.source}}};var g,A,f;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: args => {
+    const [open, setOpen] = React.useState(true);
+    return <div>
+        {open && <Alert icon={<CubeIcon />} action={<div>
+                <Button variant="text" sx={{
+          height: "35px"
+        }} onClick={() => {
+          setOpen(false);
+        }}>
+                  Close
+                </Button>
+                <IconButton color="inherit" size="small" onClick={() => {
+          setOpen(false);
+        }}>
+                  <XIcon fontSize="inherit" />
+                </IconButton>
+              </div>} {...args}>
+            {args.children}
+          </Alert>}
+        {!open && <Button onClick={() => {
+        setOpen(true);
+      }}>
+            re-open
+          </Button>}
+      </div>;
+  },
+  args: {
+    severity: "error",
+    children: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+  }
+}`,...(f=(A=a.parameters)==null?void 0:A.docs)==null?void 0:f.source}}};const pe=["AlertFloating","AlertSticky","CloseableAlert"];export{o as AlertFloating,l as AlertSticky,a as CloseableAlert,pe as __namedExportsOrder,me as default};
---
+++ assets/Tooltip.stories-Bl4IwlcK.js
@@ -0,0 +1,12 @@
+import{j as o}from"./jsx-runtime-BO8uF4Og.js";import"./lemonTheme-B7Qis2N-.js";import{w as i}from"./Rest-Calpe_Rv.js";import"./Alert-C3r4vYPZ.js";import"./Avatar-pRnr6DU5.js";import{I as n}from"./ToggleButton-1S1uagp-.js";import"./DraggableChips-Gzah4u51.js";import"./Modal-BPmM9JPO.js";import"./BottomModal-imHOmNSj.js";import"./Profile-CdAcNaFX.js";import"./Typography-BZQ6nlo4.js";import{a}from"./Taxi.es-BkFvgB_B.js";import"./index-D4H_InIO.js";import"./defaultPalette-DLKPgRCf.js";import"./dayjs.min-BJoxDa44.js";import"./CheckCircle.es-BXMbO79g.js";import"./zh-tw-C-UY_gDz.js";import"./Cart-z4MSENyy.js";import"./FormControlLabel-D0F9NOZF.js";import"./Plus.es-B6v6yg-F.js";import"./Moon.es-CJIcfKlP.js";import"./Users.es-DGjqvHPG.js";import"./WarningCircle.es-x_3DymOb.js";import"./index-Bm8UbI65.js";import"./useTheme-B9a50qyY.js";import"./useSlotProps-BPBm8KYw.js";import"./Link-CuGHZV3F.js";import"./index-BfevDTIX.js";import"./getThemeProps-MPmnPgUq.js";import"./CaretUp.es-B8wKLcLK.js";import"./MagnifyingGlassPlus.es-CjjA4OJ9.js";import"./Tabs-BBXbhsQj.js";import"./CrownSimple.es-N3wLHKBe.js";import"./Warning.es-BRl635yR.js";import"./Divider-DR1e4bhK.js";import"./Collapse-DpSy8Sr3.js";import"./Copy.es-Cj1k5QIq.js";import"./index-BkuToD7M.js";import"./index-vYCkCKEW.js";import"./tiny-invariant-CopsF_GD.js";const W={title:"components/Atoms/Tooltip/Tooltip",component:i,parameters:{layout:"centered",docs:{description:{component:"因為storybook會阻擋首次Hover行為，點擊或focus後才能正常使用"}}},tags:["autodocs"],argTypes:{placement:{control:"select",options:["top","top-start","top-end","right","right-start","right-end","bottom","bottom-start","bottom-end","left","left-start","left-end"]},arrow:{control:"boolean"}},args:{}},t={render:e=>o.jsx(i,{...e,children:o.jsx(n,{children:o.jsx(a,{})})}),args:{arrow:!0,title:"Label",placement:"bottom"}};var r,p,m;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: args => <Tooltip {...args}>
+      <IconButton>
+        <TaxiIcon />
+      </IconButton>
+    </Tooltip>,
+  args: {
+    arrow: true,
+    title: "Label",
+    placement: "bottom"
+  }
+}`,...(m=(p=t.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};const X=["Label"];export{t as Label,X as __namedExportsOrder,W as default};
